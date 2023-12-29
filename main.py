832a42e062e4b7f4272d8cbf9934eb8821244864
ChristosKyritsis <xristoskyrxk@gmail.com>
2023-12-29 16:46:24 +0200

    reading files

---
+++ main.py
@@ -0,0 +1,54 @@
+variables = {}
+structures = {}
+constraints = []
+
+def read_instance_file(file_path):
+    # File name only
+    file_name = file_path.split("/")[-1]  
+    
+    # Checking what type of file we're dealing with because there is a function for each one
+    if file_name.startswith("var"):
+        read_var_file(file_path)
+    elif file_name.startswith("dom"):
+        read_dom_file(file_path)
+    elif file_name.startswith("ctr"):
+        read_ctr_file(file_path)
+
+# Function for "var" type file
+def read_var_file(file_path):
+    with open(file_path, "r") as file:
+        lines = file.readlines()[1:]
+        for line in lines:
+            var_id, domain_id = map(int, line.split())
+            variables[var_id] = domain_id
+
+# Function for "dom" type file
+def read_dom_file(file_path):
+    with open(file_path, "r") as file:
+        lines = file.readlines()[1:]
+        for line in lines:
+            domain_id, num_values, *values = map(int, line.split())
+            structures[domain_id] = values
+
+# Function for "ctr" type file
+def read_ctr_file(file_path):
+    with open(file_path, "r") as file:
+        lines = file.readlines()[1:]
+        for line in lines:
+            constraints.append(line.strip())
+
+def main():
+    file_path = input("Enter file name: ")
+
+    try:
+        read_instance_file(file_path)
+        print("Variables:", variables)
+        print("Structs:", structures)
+        print("Constraints:", constraints)
+    except FileNotFoundError:
+        print(f"File {file_path} not found.")
+    except Exception as e:
+        print(f"Error: {e}")
+
+if __name__ == "__main__":
+    main()
